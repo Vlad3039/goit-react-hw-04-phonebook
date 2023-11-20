bb13ddee4c27995a42f0d4c02e573eac856d224d
Vlad3039 <130600836+Vlad3039@users.noreply.github.com>
2023-11-20 20:12:42 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,16 +10,18 @@ import { ContactForm } from './Form/Form';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactsList/ContactList';
 
+const initialState = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export function App() {
-  const [contacts, setContacts] = useHookLS(
-    [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    []
-  );
+  const [contacts, setContacts] = useState(() => {
+    const contactsList = JSON.parse(localStorage.getItem('contactsList'));
+    return contactsList || initialState;
+  });
 
   const [filter, setFilter] = useState('');
 
